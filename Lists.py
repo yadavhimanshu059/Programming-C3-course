c86528874b98ab47c8c0b8af14d9b0098994efae
Himanshu Yadav <yadavhimanshu059@gmail.com>
2024-06-05 18:13:49 +0530

    Add files via upload

--- Lists.py
+++ Lists.py
@@ -1,3 +1,5 @@
+from itertools import repeat
+
 a = [1,2,"a",3,4]
 print(a)
 
@@ -13,18 +15,63 @@ def Range(a):
 
 a = [1,2,4,6,2,9]
 range_a = Range(a)
+print("The range is "+str(range_a))
+
 
 mean_a = sum(a)/len(a)
 print(mean_a)
 
-print(a[0:3])
+#print(a[0:3])
 
 a = list(range(1,101))
 print(a[1])
 print(list(a))
 
 a[34] = "Book"
-print(a)
+#print(a)
+
+
+a.append(101)
+a.append(102)
+
+#print(a)
+
+
+a = [1,2,3,4]
+b = [5,6,7,8]
+c = [9,10,11,12]
+
+d = [a,b,c]
+print(d)
+
+print(d[1][1])
+
+def max_val(x,y):
+    return max([x,y])
+
+def diff_val(x,y):
+    return x-y
+
+print(map(a,b))
+print(diff_val(2,3))
+print(list(map(diff_val,a,b)))
+
+print(set(map(diff_val,a,b)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ####################
 
--- Loops.py
+++ Loops.py
@@ -1,7 +1,31 @@
+from itertools import repeat
+
+
 a = [2.4, 5, 6.7, 3, 9]
+
+print("The 1th element is "+str(a[0]))
+print("The 2th element is "+str(a[1]))
+print("The 3th element is "+str(a[2]))
+print("The 4th element is "+str(a[3]))
+print("The 5th element is "+str(a[4]))
 
 for x in a:
     print("The element is "+str(x))
+
+ids = [1,2,3,4,5]
+
+for idd in ids:
+  print("The "+str(idd)+"th element is "+str(a[idd-1]))     
+
+def concat(x,y):
+    return str(x)+str(y)
+
+
+id_names = list(map(concat,ids,["st","nd","rd"]+list(repeat("th",len(ids)-3))))
+print(id_names)
+
+for idd in ids:
+  print("The "+str(id_names[idd-1])+" element is "+str(a[idd-1]))     
 
 def summation(a):
     sum_value = 0
@@ -13,24 +37,25 @@ sum_a = summation(a)
 print(sum_a)
 
 
-def Length(a):
-    len_value = 0
-    for x in a:
-        len_value = len_value + 1
-    return len_value
-
-len_a = Length(a)
-print(len_a)
-
-a = [200,205,210,10,5000,240,300]
-
-def new_summation(a):
-    sum_value=0
-    for x in a:
-        if x==10 and x<1000:
-            sum_value = sum_value + x
-        else:
-            sum_value = sum_value
-    return sum_value
-        
-
+##def Length(a):
+##    len_value = 0
+##    for x in a:
+##        len_value = len_value + 1
+##    return len_value
+##
+##len_a = Length(a)
+##print(len_a)
+##
+##a = [200,205,210,10,5000,240,300]
+##
+##def new_summation(a):
+##    sum_value=0
+##    for x in a:
+##        if x==10 and x<1000:
+##            sum_value = sum_value + x
+##        else:
+##            sum_value = sum_value
+##    return sum_value
+##        
+##
+##
